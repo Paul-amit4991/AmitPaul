35caf9455b02cec27293c9c844239c674ee3cd21
Paul-amit4991 <55041325+Paul-amit4991@users.noreply.github.com>
2019-09-18 23:23:45 +0600

    add_project_ignore_file

---
+++ CoffeShop02/CoffeShop02/CoffeShop02.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace CoffeShop02
+{
+    partial class CoffeShop02
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.contactTextBox = new System.Windows.Forms.TextBox();
+            this.adressTextBox = new System.Windows.Forms.TextBox();
+            this.quantityTextBox = new System.Windows.Forms.TextBox();
+            this.orderComboBox = new System.Windows.Forms.ComboBox();
+            this.displayRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.showButton = new System.Windows.Forms.Button();
+            this.display02RichTextBox = new System.Windows.Forms.RichTextBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Baskerville Old Face", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(48, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Baskerville Old Face", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(48, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 14);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Contact : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Baskerville Old Face", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(48, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 14);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Adress :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Baskerville Old Face", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(48, 203);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 14);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Order :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Baskerville Old Face", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(51, 244);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 14);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Quantity :";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Font = new System.Drawing.Font("Bell MT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveButton.Location = new System.Drawing.Point(12, 304);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 5;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Font = new System.Drawing.Font("Baskerville Old Face", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameTextBox.Location = new System.Drawing.Point(125, 51);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(179, 22);
+            this.nameTextBox.TabIndex = 6;
+            // 
+            // contactTextBox
+            // 
+            this.contactTextBox.Font = new System.Drawing.Font("Baskerville Old Face", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.contactTextBox.Location = new System.Drawing.Point(125, 90);
+            this.contactTextBox.Name = "contactTextBox";
+            this.contactTextBox.Size = new System.Drawing.Size(179, 22);
+            this.contactTextBox.TabIndex = 6;
+            // 
+            // adressTextBox
+            // 
+            this.adressTextBox.Font = new System.Drawing.Font("Baskerville Old Face", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.adressTextBox.Location = new System.Drawing.Point(125, 145);
+            this.adressTextBox.Name = "adressTextBox";
+            this.adressTextBox.Size = new System.Drawing.Size(179, 22);
+            this.adressTextBox.TabIndex = 6;
+            // 
+            // quantityTextBox
+            // 
+            this.quantityTextBox.Font = new System.Drawing.Font("Baskerville Old Face", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quantityTextBox.Location = new System.Drawing.Point(125, 241);
+            this.quantityTextBox.Name = "quantityTextBox";
+            this.quantityTextBox.Size = new System.Drawing.Size(179, 25);
+            this.quantityTextBox.TabIndex = 6;
+            // 
+            // orderComboBox
+            // 
+            this.orderComboBox.Font = new System.Drawing.Font("Baskerville Old Face", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.orderComboBox.FormattingEnabled = true;
+            this.orderComboBox.Items.AddRange(new object[] {
+            "Black",
+            "Cold",
+            "Hot",
+            "Regular"});
+            this.orderComboBox.Location = new System.Drawing.Point(125, 200);
+            this.orderComboBox.Name = "orderComboBox";
+            this.orderComboBox.Size = new System.Drawing.Size(175, 22);
+            this.orderComboBox.TabIndex = 7;
+            // 
+            // displayRichTextBox
+            // 
+            this.displayRichTextBox.Font = new System.Drawing.Font("High Tower Text", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayRichTextBox.Location = new System.Drawing.Point(375, 12);
+            this.displayRichTextBox.Name = "displayRichTextBox";
+            this.displayRichTextBox.Size = new System.Drawing.Size(221, 125);
+            this.displayRichTextBox.TabIndex = 8;
+            this.displayRichTextBox.Text = "";
+            // 
+            // showButton
+            // 
+            this.showButton.Font = new System.Drawing.Font("Bell MT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.showButton.Location = new System.Drawing.Point(103, 304);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 9;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // display02RichTextBox
+            // 
+            this.display02RichTextBox.Font = new System.Drawing.Font("High Tower Text", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.display02RichTextBox.Location = new System.Drawing.Point(375, 165);
+            this.display02RichTextBox.Name = "display02RichTextBox";
+            this.display02RichTextBox.Size = new System.Drawing.Size(221, 134);
+            this.display02RichTextBox.TabIndex = 10;
+            this.display02RichTextBox.Text = "";
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("Bell MT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(196, 304);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 11;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Font = new System.Drawing.Font("Bell MT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.closeButton.Location = new System.Drawing.Point(277, 304);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 12;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Algerian", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(100, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(193, 18);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Customer Information";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Bodoni MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(451, 309);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(85, 17);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Customer List";
+            // 
+            // CoffeShop02
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 356);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.display02RichTextBox);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.displayRichTextBox);
+            this.Controls.Add(this.orderComboBox);
+            this.Controls.Add(this.quantityTextBox);
+            this.Controls.Add(this.adressTextBox);
+            this.Controls.Add(this.contactTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Baskerville Old Face", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "CoffeShop02";
+            this.Text = "CoffeShop02";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox contactTextBox;
+        private System.Windows.Forms.TextBox adressTextBox;
+        private System.Windows.Forms.TextBox quantityTextBox;
+        private System.Windows.Forms.ComboBox orderComboBox;
+        private System.Windows.Forms.RichTextBox displayRichTextBox;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.RichTextBox display02RichTextBox;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ CoffeShop02/CoffeShop02/CoffeShop02.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoffeShop02
+{
+    public partial class CoffeShop02 : Form
+    {
+        List<string> names = new List<string> { };
+        List<string> contacts = new List<string> { };
+        List<string> adress  = new List<string> { };
+        List<string> orders = new List<string> { };
+        List<int> quantitys = new List<int> { };
+       
+        int price = 0;
+        public CoffeShop02()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+           
+            if (!string.IsNullOrEmpty(nameTextBox.Text))
+            {
+                names.Add(nameTextBox.Text);
+            }
+            else
+            {
+                MessageBox.Show("Please Select a Name..!!!");
+                return;
+            }
+
+            if (!string.IsNullOrEmpty(contactTextBox.Text))
+            {
+                contacts.Add(contactTextBox.Text);    
+                }
+            else
+            {
+                MessageBox.Show("Please Give Contact Number..!!!");
+                return;
+            }
+            if (!string.IsNullOrEmpty(adressTextBox.Text))
+            {
+                adress.Add(adressTextBox.Text);
+            }
+            else
+            {
+                MessageBox.Show("Please Give Adress..!!!");
+                return;
+            }
+            if (!string.IsNullOrEmpty(orderComboBox.Text))
+            {
+                orders.Add(orderComboBox.Text);
+            }
+            else
+            {
+                MessageBox.Show("Please Select an Item..!!!");
+                return;
+            }
+            if (!string.IsNullOrEmpty(quantityTextBox.Text))
+            {
+                quantitys.Add(Convert.ToInt32(quantityTextBox.Text));
+            }
+            else
+            {
+                MessageBox.Show("Please give the quantity..!!!");
+                return;
+            }
+            if ((orderComboBox.Text) == "Black")
+            {
+                price = Convert.ToInt32(quantityTextBox.Text) * 120;
+            }
+            else if ((orderComboBox.Text) == "Cold")
+            {
+                price = Convert.ToInt32(quantityTextBox.Text) * 100;
+            }
+            else if ((orderComboBox.Text) == "Hot")
+            {
+                price = Convert.ToInt32(quantityTextBox.Text) * 90;
+            }
+            else if ((orderComboBox.Text) == "Regular")
+            {
+                price = Convert.ToInt32(quantityTextBox.Text) * 80;
+            }
+             string Message = "";
+            for (int i = 0; i < names.Count(); i++)
+            {
+                Message = "Name : " + names[i] + "\n" + "Contact : " + contacts[i] + "\n" + "Orders : " + orders[i] + " \n" + "Quantity : " + quantitys[i] + "\n" + "Price: " + price + "\n\n";
+            }
+
+            displayRichTextBox.AppendText(Message);
+
+            nameTextBox.Text = "";
+            contactTextBox.Text = "";
+            adressTextBox.Text = "";
+            orderComboBox.Text = "select item";
+            quantityTextBox.Text = "";
+            nameTextBox.Focus();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            
+            for (int i = 0; i < names.Count(); i++)
+            {
+                
+                display02RichTextBox.AppendText("Name : " + names[i] + "\n" + "Contact : " + contacts[i] + "\n" + "Orders : " + orders[i] + " \n" + "Quantity : " + quantitys[i] + "\n" + "Price: " + price + "\n\n");
+                
+            }
+            displayRichTextBox.Clear();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            nameTextBox.Text = "";
+            contactTextBox.Text = "";
+            adressTextBox.Text = "";
+            orderComboBox.Text = "select item";
+            quantityTextBox.Text = "";
+            nameTextBox.Focus();
+
+            displayRichTextBox.Clear();
+            display02RichTextBox.Clear();
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
